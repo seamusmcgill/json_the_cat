ac8dd249785b56fd67ef1f2c25656d2b1a3a9112
Seamus McGill <seamusmcgill@gmail.com>
2022-01-20 15:00:20 -0500

    Refactored breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,19 +2,18 @@ const request = require("request");
 
 const fetchBreedDescription = (breedName, callback) => {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-    const data = JSON.parse(body);
+    const [data] = JSON.parse(body);
     // Output the error message if URL isn't valid
     if (error) {
-      callback(data[0].message, null);
+      callback(data.message, null);
     }
     if (!error) {
       // If no error but no breed was found
-      if (data.length === 0) {
-        callback("Breed not found.", null);
-      } else if (data) {
-      // If breed was entered output description
-        callback(null, data[0].description);
+      if (data) {
+        return callback(null, data.description);
       }
+      // If breed was entered output description
+      callback("Breed not found.", null);
     }
   });
 };
